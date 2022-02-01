e1c524cc1c6a42fb638fbf47e2d0e0eb200d9f6e
David H <cpif@handysoftware.com>
2022-01-31 23:00:55 -0500

    Fixed the crashing bug by clearing the PWM interrupt bit

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,22 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
 use core::fmt::Write; // for write!(), writeln!()
+use cortex_m::interrupt::Mutex;
 use cortex_m_rt::entry;
 use embedded_hal::digital::v2::OutputPin;
 use embedded_time::rate::*;
 use panic_halt as _;
-use rp_pico::hal::{self, pac::interrupt, prelude::*};
+use rp_pico::hal::{
+    self,
+    pac::interrupt,
+    prelude::*,
+    pwm::{FreeRunning, Pwm4, Slice},
+};
+
+type PwmSlice = Slice<Pwm4, FreeRunning>;
+static PWM4: Mutex<RefCell<Option<PwmSlice>>> = Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
@@ -40,10 +50,10 @@ fn main() -> ! {
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
 
     // Init PWMs
-    let mut pwm_slices = hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
+    let pwm_slices = hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
 
     // Configure PWM4 for 50.0288 Hz when fsys is 125MHz
-    let pwm = &mut pwm_slices.pwm4;
+    let mut pwm = pwm_slices.pwm4;
     pwm.default_config();
     pwm.set_div_int(38);
     pwm.set_div_frac(2);
@@ -80,22 +90,26 @@ fn main() -> ! {
     // LED for diagnostic signals
     let mut led = pins.led.into_push_pull_output();
 
-    // Running this block of code enables the PWM_IRQ_WRAP interrupt handler
-    // function to be called. Unfortunately, whenever PWM_IRQ_WRAP() is called
-    // the main program crashes immediately. Even if the function is empty.
-    //
-    // Change this to ``true`` to cause the crash:
-    if false {
-        // Tell the NVIC to start calling the PWM interrupt handler function
-        unsafe {
-            hal::pac::NVIC::unmask(hal::pac::Interrupt::PWM_IRQ_WRAP);
-        }
+    // Tell the NVIC to start calling the PWM interrupt handler function
+    unsafe {
+        hal::pac::NVIC::unmask(hal::pac::Interrupt::PWM_IRQ_WRAP);
     }
 
     // Enable the interrupt for this PWM slice
     pwm.enable_interrupt();
-    // Start the PWM clock running
-    pwm.enable();
+
+    cortex_m::interrupt::free(move |cs| {
+        // Put this pwm slice into a static struct accessible from the
+        // interrupt handler.
+        PWM4.borrow(cs).replace(Some(pwm));
+        // Borrow the pwm slice back from the static struct so we can
+        // start the PWM clock running.
+        let pwm_ref = PWM4.borrow(cs);
+        let mut pwm_mut = pwm_ref.borrow_mut();
+        let pwm = pwm_mut.as_mut().unwrap();
+        // Start the PWM clock running
+        pwm.enable();
+    });
 
     let t0 = timer.get_counter();
     let mut t1 = t0;
@@ -116,6 +130,18 @@ fn main() -> ! {
     }
 }
 
-/// PWM interrupt handler function. Notice it is empty and should be harmless.
+/// PWM interrupt handler function.
 #[interrupt]
-fn PWM_IRQ_WRAP() {}
+fn PWM_IRQ_WRAP() {
+    // We MUST grab the pwm slice out of the static struct and clear the
+    // interrupt. Otherwise, this handler function will be called again
+    // immediately upon returning - forever.
+    cortex_m::interrupt::free(move |cs| {
+        let pwm_ref = PWM4.borrow(cs);
+        let mut pwm_mut = pwm_ref.borrow_mut();
+        if let Some(ref mut pwm) = pwm_mut.as_mut() {
+            // If you comment-out this line, this program will crash:
+            pwm.clear_interrupt();
+        }
+    });
+}
